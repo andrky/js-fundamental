2deed2d52e91dc979c347482838b77766d76b5f1
Ananda Rizky <anandarizky166@gmail.com>
2023-12-12 16:46:23 +0700

    sampaiObjectdanArray

--- mini-project-1/starter/app.js
+++ mini-project-1/starter/app.js
@@ -1,3 +1,34 @@
-const heading = document.querySelector(".heading-title");
+const heading = document.querySelector('.heading-title');
 
-heading.innerHTML = "Title Baru";
+heading.innerHTML = 'Title Baru'; 
+
+let kali = 500 * (7 + 5) + 6
+
+console.log(kali)
+
+let number = "3" + "3" - "tes";
+
+console.log(number)
+
+let biodataDiri = {
+	ayah: {
+		nama: 'Doni',
+		umur: 52,
+		jenisKelamin: 'Pria',
+		makananFavorit: ['Burger', 'Pisang'],
+	},
+	ibu: {
+		nama: 'Sinta',
+		umur: 43,
+		jenisKelamin: 'Wanita',
+		makananFavorit: ['Soto', 'Donat'],
+	},
+	anak: {
+		nama: 'Deri',
+		umur: 7,
+		jenisKelamin: 'Pria',
+		makananFavorit: ['Es', 'Nugget'],
+	},
+};
+
+console.log(biodataDiri.ayah.makananFavorit[1])
