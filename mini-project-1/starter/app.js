5c41a4b9a13c0329ad0834ecaa5964427390bfaf
DESKTOP-MAF5T6N\Darky <anandarizky166@gmail.com>
2023-12-14 18:05:13 +0700

    sampaiArrayProperty

--- mini-project-1/starter/app.js
+++ mini-project-1/starter/app.js
@@ -1,34 +1,11 @@
-const heading = document.querySelector('.heading-title');
+const changeTitle = title => {
+  const heading = document.querySelector('.heading-title');
 
-heading.innerHTML = 'Title Baru'; 
+  if (typeof title == 'string') {
+    heading.innerHTML = title; 
+  } else {
+    alert('Tidak bisa rubah judul')
+  }
+}
 
-let kali = 500 * (7 + 5) + 6
-
-console.log(kali)
-
-let number = "3" + "3" - "tes";
-
-console.log(number)
-
-let biodataDiri = {
-	ayah: {
-		nama: 'Doni',
-		umur: 52,
-		jenisKelamin: 'Pria',
-		makananFavorit: ['Burger', 'Pisang'],
-	},
-	ibu: {
-		nama: 'Sinta',
-		umur: 43,
-		jenisKelamin: 'Wanita',
-		makananFavorit: ['Soto', 'Donat'],
-	},
-	anak: {
-		nama: 'Deri',
-		umur: 7,
-		jenisKelamin: 'Pria',
-		makananFavorit: ['Es', 'Nugget'],
-	},
-};
-
-console.log(biodataDiri.ayah.makananFavorit[1])
+changeTitle("Keren Gan");
---
+++ mini-project-1/starter/app2.js
@@ -0,0 +1,62 @@
+const array = [1,2,3,4,5,6];
+
+for (let i = 0; i <= array.length; i++){
+  console.log(array[i]);
+}
+
+// const heading = document.querySelector('.heading-title');
+
+// heading.innerHTML = 'Title Baru'; 
+
+
+// // Function Definition
+// const perpangkatan = function (n) {
+//   return n * n;
+// }
+
+// // Function Declaration
+// function pangkat(n) {
+//   return n * n;
+// }
+
+// function sayHello(n) {
+//   alert('Halo');
+
+//   return n;
+// }
+
+// const tes = n => n * n;
+
+
+// console.log(perpangkatan(5));
+
+// // let kali = 500 * (7 + 5) + 6
+
+// // console.log(kali)
+
+// // let number = "3" + "3" - "tes";
+
+// // console.log(number)
+
+// // let biodataDiri = {
+// // 	ayah: {
+// // 		nama: 'Doni',
+// // 		umur: 52,
+// // 		jenisKelamin: 'Pria',
+// // 		makananFavorit: ['Burger', 'Pisang'],
+// // 	},
+// // 	ibu: {
+// // 		nama: 'Sinta',
+// // 		umur: 43,
+// // 		jenisKelamin: 'Wanita',
+// // 		makananFavorit: ['Soto', 'Donat'],
+// // 	},
+// // 	anak: {
+// // 		nama: 'Deri',
+// // 		umur: 7,
+// // 		jenisKelamin: 'Pria',
+// // 		makananFavorit: ['Es', 'Nugget'],
+// // 	},
+// // };
+
+// // console.log(biodataDiri.ayah.makananFavorit[1])
